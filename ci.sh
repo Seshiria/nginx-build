f4c2cb8d538ae51560ef744d3fbd1c842eecafbc
Seshiria <57834968+Seshiria@users.noreply.github.com>
2024-07-03 20:13:37 +0800

    切换到构建ubuntu的deb

--- ci.sh
+++ ci.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 set -ex
 cd ${GITHUB_WORKSPACE}
-docker pull centos:7
 docker build -t nginx-build .
-docker run --name nginx-build nginx-build sh -x /file/run.sh
-docker cp nginx-build:/root/rpmbuild/RPMS/x86_64/ ./
+docker run --name nginx-build nginx-build bash -x /file/run.sh
+docker cp nginx-build:/root/build/release/ ./
--- file/run.sh
+++ file/run.sh
@@ -5,46 +5,20 @@ if [ -f /file/install.so ];then
     mv /file/install.so /file/install
     exit 0
 fi
-mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-cd ~/rpmbuild/SOURCES/
-rm -rf ~/rpmbuild/SOURCES/*
+mkdir -p ~/build/
+cd ~/build/
+rm -rf ~/build/*
 #获取nginx源码
-NGINX_VERSION=$(yum list nginx |grep nginx |awk -F ':' '{print $2}'|awk -F ' ' '{print $1}')
-if [ -f /file/nginx-${NGINX_VERSION}.src.rpm ];then
-    cp /file/nginx-${NGINX_VERSION}.src.rpm ./
-else
-    curl -O http://nginx.org/packages/centos/7/SRPMS/nginx-${NGINX_VERSION}.src.rpm
-fi
-rpm2cpio nginx-${NGINX_VERSION}.src.rpm |cpio -dvi
-#hook nginx.spec 文件
-#hook %setup宏
-#sed -i '/cp %{SOURCE2} ./a\. /file/hook.sh' nginx.spec
-if `grep -q "%autosetup" nginx.spec` ;then
-    #nginx version 1.20.0 and up
-    sed -i -e '/%autosetup -p1/a\mv ./configure ./configured \ncp /file/configure ./configure \nchmod +x ./configure\n#hockpoint' \
-            -e '/%build/a\export BASE_CONFIGURE_ARGS=%{BASE_CONFIGURE_ARGS} \nexport WITH_CC_OPT="\%{WITH_CC_OPT}\" \nexport WITH_LD_OPT=\"%{WITH_LD_OPT}\"' \
-                nginx.spec
-elif `grep -q "%setup" nginx.spec ` ;then
-    #nginx version 1.18.0 and lower
-    sed -i -e '/cp %{SOURCE2} ./a\mv ./configure ./configured \ncp /file/configure ./configure \nchmod +x ./configure\n#hockpoint' \
-        -e '/%build/a\export BASE_CONFIGURE_ARGS=%{BASE_CONFIGURE_ARGS} \nexport WITH_CC_OPT="\%{WITH_CC_OPT}\" \nexport WITH_LD_OPT=\"%{WITH_LD_OPT}\"' \
-            nginx.spec
-else
-    echo "error:Unsupported nginx.spec, the following information may be useful"
-    echo "--------------------------------------------"
-    #cat nginx.spec
-    echo "--------------------------------------------"
-    exit 1
-fi
-#check
-if `grep -q "#hockpoint" nginx.spec` ;then
-    echo "hock point check is ok"
-else
-    echo "error: hock point not written"
-    echo "Please check nginx.spec,the following information may be useful"
-    echo "--------------------------------------------"
-    cat nginx.spec
-    echo "--------------------------------------------"
-    exit 1
-fi
-rpmbuild -ba nginx.spec
+apt update
+NGINX_VERSION=$(apt list nginx |grep nginx |awk -F ' ' '{print $2}'|awk -F '~' '{print $1}')
+NGINX_VERSION_MAIN=$(echo "$NGINX_VERSION" |awk -F '-' '{print $1}')
+apt source nginx
+cd nginx-"${NGINX_VERSION_MAIN}"
+
+. /file/configure
+
+dpkg-buildpackage -uc -b
+#发布文件
+cd ~/build/
+mkdir release
+mv *.deb release/
